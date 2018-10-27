bdd94fd00fd1087c23970191ebbfeef85a272bf5
Vincent <vincentquan64@gmail.com>
2018-10-26 20:02:14 -0700

    new sample

--- database/seed.js
+++ database/seed.js
@@ -14,15 +14,65 @@ const sampleData = [
         imgDescription: 'This is another room.',
       },
       {
-        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/2.jpg',
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/3.jpg',
         imgDescription: 'This is another room.',
       },
       {
-        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/2.jpg',
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/4.jpg',
         imgDescription: 'This is another room.',
       },
       {
-        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/2.jpg',
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/5.jpg',
+        imgDescription: 'This is another room.',
+      },
+    ],
+  },
+  {
+    title: 'Hohenzollern Castle',
+    imgs: [
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/6.jpg',
+        imgDescription: 'This is a room.',
+      },
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/7.jpg',
+        imgDescription: 'This is another room.',
+      },
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/8.jpg',
+        imgDescription: 'This is another room.',
+      },
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/9.jpg',
+        imgDescription: 'This is another room.',
+      },
+    ],
+  },
+  {
+    title: 'Heidelberg Castle',
+    imgs: [
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/10.jpg',
+        imgDescription: 'This is a room.',
+      },
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/11.jpg',
+        imgDescription: 'This is another room.',
+      },
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/12.jpg',
+        imgDescription: 'This is another room.',
+      },
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/13.jpg',
+        imgDescription: 'This is another room.',
+      },
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/14.jpg',
+        imgDescription: 'This is another room.',
+      },
+      {
+        imgURL: 'https://s3-us-west-1.amazonaws.com/lairbnb-carousel/15.jpg',
         imgDescription: 'This is another room.',
       },
     ],
@@ -30,8 +80,13 @@ const sampleData = [
 ];
 
 const insertSampleImgs = function() {
-  Carousel.create(sampleData)
+  const randomInt = () => {
+    return Math.floor(Math.random() * Math.floor(3))
+  };
+  for (var i = 0; i < 2; i++) {
+    Carousel.create(sampleData[randomInt()])
     .then(() => db.disconnect());
+  }
 };
 
 insertSampleImgs();
