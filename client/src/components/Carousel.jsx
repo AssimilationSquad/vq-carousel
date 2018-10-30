9e625b9d2d40e701d5973bc90537ec57a5b0cffd
Vincent <vincentquan64@gmail.com>
2018-10-30 15:55:34 -0700

    arrow buttons set state

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -7,29 +7,51 @@ class Carousel extends React.Component {
     this.state = {
       imgs: [],
       imgDisplay: '',
+      imgIndex: '',
+      result: '',
     }
-
     //function bindings go here
+    this.nextImage = this.nextImage.bind(this);
+    this.lastImage = this.lastImage.bind(this);
   }
+
+  nextImage() {
+    const newIndex = this.state.imgIndex + 1;
+    this.setState({
+      imgIndex: newIndex,
+      imgDisplay: this.state.result[0].imgs[newIndex].imgURL,
+    })
+  }
+  lastImage() {
+    const newIndex = this.state.imgIndex - 1;
+    this.setState({
+      imgIndex: newIndex,
+      imgDisplay: this.state.result[0].imgs[newIndex].imgURL,
+    })
+  }
+
   componentDidMount() {
     fetch("http://localhost:3004/rooms/16")
       .then(res => res.json())
       .then(
         (result) => {
           this.setState({
+            result: result,
             imgs: result[0].imgs,
             imgDisplay: result[0].imgs[0].imgURL,
+            imgIndex: 0,
           })
         }
       )
   }
 
+
   render() {
     return (
       <div>
-        <button>◀</button>
+        <button className="lastImg" onClick={this.lastImage}>◀</button>
         <div className="slideDisplay"><img src={this.state.imgDisplay}></img></div>
-        <button>▶</button>
+        <button className="nextImg" onClick={this.nextImage} >▶</button>
       </div>
     )
   }
