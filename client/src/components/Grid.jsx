56c2fb35c62094d231d593b5eb34136fb0367a03
Quanquest <31722969+Quanquest@users.noreply.github.com>
2018-11-01 11:09:36 -0700

    Refactor state to minimum number of properties

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -6,11 +6,7 @@ class Grid extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mainImage: '',
-      image1: '',
-      image2: '',
-      image3: '',
-      image4: '',
+      imgs: [],
     }
   }
 
@@ -20,13 +16,7 @@ class Grid extends React.Component {
       .then(
         (result) => {
           this.setState({
-            result: result,
             imgs: result[0].imgs,
-            mainImage: result[0].imgs[0].imgURL,
-            image1: result[0].imgs[1].imgURL,
-            image2: result[0].imgs[2].imgURL,
-            image3: result[0].imgs[3].imgURL,
-            image4: result[0].imgs[4].imgURL,
           })
         }
       )
@@ -36,19 +26,19 @@ class Grid extends React.Component {
     return (
       <div className={gridStyles.gridcontainer}>
         <div className={gridStyles.mainImage}>
-          <img src={this.state.mainImage}></img>
+          <img src={this.state.imgs[0].imgURL}></img>
         </div>
         <div className={gridStyles.image1}>
-          <img src={this.state.image1}></img>
+          <img src={this.state.imgs[1].imgURL}></img>
         </div>
         <div className={gridStyles.image2}>
-          <img src={this.state.image2}></img>
+          <img src={this.state.imgs[2].imgURL}></img>
         </div>
         <div className={gridStyles.image3}>
-          <img src={this.state.image3}></img>
+          <img src={this.state.imgs[3].imgURL}></img>
         </div>
         <div className={gridStyles.image4}>
-          <img src={this.state.image4}></img>
+          <img src={this.state.imgs[4].imgURL}></img>
         </div>
       </div>    
     );
