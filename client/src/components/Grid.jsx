c1613f2375b5a5acae149b9808afca44f52fa708
Vincent <vincentquan64@gmail.com>
2018-11-05 16:30:17 -0800

    cors

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -39,7 +39,7 @@ class Grid extends React.Component {
   componentDidMount() {
     const url = window.location.pathname;
     if (url !== '/') {
-        fetch(`http://localhost:3004/api${url}`)
+        fetch(`http://127.0.0.1:3004/api${url}`)
           .then(res => res.json())
           .then(
             (result) => {
